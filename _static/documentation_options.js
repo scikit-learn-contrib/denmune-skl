0353e7a86fa3d367d27e2771b19d9817cf7fb76b
shivvor2 <shivvor2@users.noreply.github.com>
2025-12-20 05:28:45 +0000

    [ci skip] Merge pull request #8 from scikit-learn-contrib/refactor/refactor-fit
    Refactor: Refactored fit method by extracting sub-algo logic 2d42fe04def466a4ca25cca7214d444971487a04

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev8+g34608e43f',
+    VERSION: '0.1.4.dev5+g2d42fe04d.d20251220',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
